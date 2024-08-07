8b80614173c55d9b777f8d0916c84aa302a3cd2c
saranyasidharth <saranyasidhartht@gmail.com>
2024-08-07 18:40:29 +0530

    Added dashboard and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+
+import "./App.css";
+import PrimarySearchAppBar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import theme from "./config/theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <PrimarySearchAppBar />
+      <div className="dashboard">
+        <Dashboard />
+      </div>
+    </ThemeProvider>
   );
 }
 
